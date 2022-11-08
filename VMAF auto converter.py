087fb1d9b0d608f088cc51896561af866a10367f
Nicklas <nicklashelsberg@gmail.com>
2022-11-08 13:55:46 +0100

    Swapped VMAF min and max to variables for easier user preference

--- VMAF auto converter.py
+++ VMAF auto converter.py
@@ -10,20 +10,22 @@ except:
     pass
 for file in glob.glob('*.mp4'):
     vmaf_value = 0
-    crf_value = 34
+    crf_value = 34 #Change this to set the default CRF value for ffmpeg to start converting with
+    VMAF_min_value = 90 #Change this to determine the minimum allowed VMAF quality
+    VMAF_max_value = 95 #Change this to determine the maximum allowed VMAF quality
     while True:
         subprocess.run(['ffmpeg', '-n', '-i', file, '-c:a', 'aac', '-c:v', 'libsvtav1', '-crf', str(crf_value), '-b:v', '0', '-b:a', '192k', f'converted/{os.path.splitext(file)[0]} converted{os.path.splitext(file)[1]}'])
         subprocess.run(['ffmpeg', '-i', f'converted/{os.path.splitext(file)[0]} converted{os.path.splitext(file)[1]}', '-i', file, '-lavfi', 'libvmaf=log_path=log.xml', '-f', 'null', '-'], stdout=subprocess.PIPE)
         root = ET.parse('log.xml').getroot()
         vmaf_value = float(root.findall('pooled_metrics/metric')[-1].get('mean'))
 
-        if not 90 <= vmaf_value <= 95:
-            if vmaf_value < 90:
+        if not VMAF_min_value <= vmaf_value <= VMAF_max_value:
+            if vmaf_value < VMAF_min_value:
                 print('\nVMAF value too low, retrying with a CRF decrease of 1...')
                 time.sleep(2)
                 crf_value = crf_value - 1
                 os.remove(f'converted/{os.path.splitext(file)[0]} converted{os.path.splitext(file)[1]}')
-            elif vmaf_value > 95:
+            elif vmaf_value > VMAF_max_value:
                 print('\nVMAF value too high, retrying with a CRF increase of 1...')
                 time.sleep(2)
                 crf_value = crf_value + 1
