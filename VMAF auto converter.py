5972c86d75b0732ee4cee767193ab4ec9443a828
Nicklas <nicklashelsberg@gmail.com>
2022-11-09 11:39:11 +0100

    Added movflag faststart to conversion

--- VMAF auto converter.py
+++ VMAF auto converter.py
@@ -6,6 +6,7 @@ import time
 
 input_dir = 'lossless' #Change this to set a custom input directory. Dot can be used to specify same directory as the script
 output_dir = 'AV1' #Change this to set a custom input directory. Dot can be used to specify same directory as the script
+#Changing both to a dot is not adviced since the original filename and extension is reused in the output, meaning ffmpeg will either outright fail, or the script can delete the file
 
 if os.name == 'nt': #Visual Studio Code will complain about either one being unreachable, since os.name is a variable. Just ignore this
     pass_1_output = 'NUL'
@@ -22,7 +23,7 @@ for file in glob.glob(f'{input_dir}{os.path.sep}*.mp4'):
     attempt = 0
     max_attempt = 10 #Change this to set the max amount of allowed retries before quitting
     crf_value = 40 #Change this to set the default CRF value for ffmpeg to start converting with
-    crf_step = 1 #Change this to set the amount the CRF value should be increased per retry
+    crf_step = 1 #Change this to set the amount the CRF value should change per retry
     VMAF_min_value = 90 #Change this to determine the minimum allowed VMAF quality
     VMAF_max_value = 95 #Change this to determine the maximum allowed VMAF quality
     while True:
@@ -31,9 +32,9 @@ for file in glob.glob(f'{input_dir}{os.path.sep}*.mp4'):
             time.sleep(2)
             break
         attempt += 1
-        p1 = subprocess.run(['ffmpeg', '-n', '-i', file, '-c:a', 'aac', '-c:v', 'libsvtav1', '-crf', str(crf_value), '-an', '-g', '600', '-preset', '8', '-pass', '1', '-f', 'null', pass_1_output])
+        p1 = subprocess.run(['ffmpeg', '-n', '-i', file, '-c:a', 'aac', '-c:v', 'libsvtav1', '-crf', str(crf_value), '-an', '-g', '600', '-preset', '8', '-movflags', '+faststart', '-pass', '1', '-f', 'null', pass_1_output])
         if p1.returncode == 0: #Skip on error or if file already exists
-            p2 = subprocess.run(['ffmpeg', '-n', '-i', file, '-c:a', 'aac', '-c:v', 'libsvtav1', '-crf', str(crf_value), '-b:v', '0', '-b:a', '192k', '-g', '600', '-preset', '8', '-pass', '2', f'{output_dir}{os.path.sep}{os.path.basename(file)}'])
+            p2 = subprocess.run(['ffmpeg', '-n', '-i', file, '-c:a', 'aac', '-c:v', 'libsvtav1', '-crf', str(crf_value), '-b:v', '0', '-b:a', '192k', '-g', '600', '-preset', '8', '-movflags', '+faststart', '-pass', '2', f'{output_dir}{os.path.sep}{os.path.basename(file)}'])
             if p2.returncode == 0: #Skip on error or if file already exists
                 subprocess.run(['ffmpeg', '-i', f'{output_dir}{os.path.sep}{os.path.basename(file)}', '-i', file, '-lavfi', f'libvmaf=log_path=log.xml:n_threads={logical_cores}', '-f', 'null', '-'])
                 root = ET.parse('log.xml').getroot() #Parse the XML file containing the VMAF value
