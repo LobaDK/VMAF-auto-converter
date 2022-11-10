f108c55f56b7209fc988043a594675bc42166f7d
Nicklas <nicklashelsberg@gmail.com>
2022-11-10 12:06:35 +0100

    Added exponential CRF increaser. Swapped to ignore file extensions when checking for duplicate file

--- VMAF auto converter.py
+++ VMAF auto converter.py
@@ -6,12 +6,11 @@ import time
 
 input_dir = 'lossless' #Change this to set a custom input directory. Dot can be used to specify same directory as the script
 output_dir = 'AV1' #Change this to set a custom input directory. Dot can be used to specify same directory as the script
-#Changing both to a dot is not adviced since the original filename and extension is reused in the output, meaning ffmpeg will either outright fail, or the script can delete the file
+#Changing both to a dot is not adviced since the original filename is reused in the output, meaning if they share the same extension, ffmpeg will either outright fail, or the script can delete the input file
 
-input_extension = 'mp4'
+input_extension = 'mp4' #Change this to set the container type that should be converted. A * (wildcard) can instead be used to ignore container type, but make sure there's only video files in the given directory then 
 logical_cores = round(os.cpu_count() / 2) # get the amount of logical cores available on system.
 max_attempt = 10 #Change this to set the max amount of allowed retries before quitting
-crf_step = 1 #Change this to set the amount the CRF value should change per retry
 VMAF_min_value = 90 #Change this to determine the minimum allowed VMAF quality
 VMAF_max_value = 95 #Change this to determine the maximum allowed VMAF quality
 
@@ -29,12 +28,13 @@ for file in glob.glob(f'{input_dir}{os.path.sep}*.{input_extension}'):
     attempt = 0
     crf_value = 40 #Change this to set the default CRF value for ffmpeg to start converting with
     while True:
+        crf_step = 1 #Change this to set the amount the CRF value should change per retry
         if attempt >= max_attempt:
             print('\nMaximum amount of allowed attempts exceeded. Stopping...')
             time.sleep(2)
             break
         attempt += 1
-        if not os.path.isfile(f'{output_dir}{os.path.sep}{os.path.basename(file)}'): #check if the same file already exists in the output folder
+        if not glob.glob(f'{output_dir}{os.path.sep}{os.path.basename(os.path.splitext(file)[0])}.*'): #check if the same filename already exists in the output folder. Extension is ignored to allow custom input container types/extensions
             p1 = subprocess.run(['ffmpeg', '-n', '-i', file, '-c:a', 'aac', '-c:v', 'libsvtav1', '-crf', str(crf_value), '-b:v', '0', '-an', '-g', '600', '-preset', '8', '-movflags', '+faststart', '-pass', '1', '-f', 'null', pass_1_output])
             if p1.returncode == 0: #Skip on error
                 p2 = subprocess.run(['ffmpeg', '-n', '-i', file, '-c:a', 'aac', '-c:v', 'libsvtav1', '-crf', str(crf_value), '-b:v', '0', '-b:a', '192k', '-g', '600', '-preset', '8', '-movflags', '+faststart', '-pass', '2', f'{output_dir}{os.path.sep}{os.path.basename(file)}'])
@@ -45,11 +45,15 @@ for file in glob.glob(f'{input_dir}{os.path.sep}*.{input_extension}'):
 
                     if not VMAF_min_value <= vmaf_value <= VMAF_max_value: #If VMAF value is not inside the VMAF range
                         if vmaf_value < VMAF_min_value: #If VMAF value is below the minimum range
+                            for _ in range(int((VMAF_min_value - vmaf_value) / 2)):
+                                crf_step += 1
                             print(f'\nVMAF value too low, retrying with a CRF decrease of {crf_step} ({crf_value - crf_step})...')
                             time.sleep(2)
                             crf_value -= crf_step
                             os.remove(f'{output_dir}{os.path.sep}{os.path.basename(file)}') #Delete converted file to avoid FFmpeg skipping it
                         elif vmaf_value > VMAF_max_value: #If VMAF value is above the maximum range
+                            for _ in range(int((vmaf_value - VMAF_max_value) / 2)):
+                                crf_step += 1
                             print(f'\nVMAF value too high, retrying with a CRF increase of {crf_step} ({crf_value + crf_step})...')
                             time.sleep(2)
                             crf_value += crf_step
